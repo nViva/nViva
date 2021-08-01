8b9a272d21eb65934e6128d07b3900c0dcfe61a2
Your Name <mbonyinshutifiston@gmail.com>
2021-08-01 14:41:32 +0200

    update

--- delete.php
+++ delete.php
@@ -7,7 +7,7 @@ if (isset($_GET['id']))
 
 $result = mysqli_query($db,"DELETE FROM Products WHERE productId=".$_GET['id']);
 if($result===true)
-	
+//if done go to Ourstock	
 header("Location:Ourstock.php");
 }
 
